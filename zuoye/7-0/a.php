2e92399691d95bd09ed71df88b2bdd06707d2443
孤寂 <15688827918@163.com>
2023-11-30 08:40:53 +0800

    1

---
+++ zuoye/7-0/a.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$username = $_POST["textfield"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+
+
+
+
+echo $username ."<br>";
+echo $password ."<br>";
+echo $email ."<br>";
